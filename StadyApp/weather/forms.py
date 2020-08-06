bde361603d4977782ebd1c31ea2880283f6e15ed
Nes Kolins <45214736+sdfvb@users.noreply.github.com>
2020-08-07 00:12:28 +0400

    Weather

--- StadyApp/weather/forms.py
+++ StadyApp/weather/forms.py
@@ -1,9 +1,12 @@
+from django.core.exceptions import ValidationError
+
 from .models import City
 from django.forms import ModelForm, TextInput
 
 
 class CityForm(ModelForm):
     """ Формы джанго """
+
     class Meta:
         model = City
         fields = ['name']
@@ -12,3 +15,11 @@ class CityForm(ModelForm):
                                             'id': 'city',
                                             'placeholder': 'Введите город'
                                             })}
+
+    def clean_name(self):
+        new_name = self.cleaned_data['name']
+
+        if City.objects.filter(name=new_name).count():
+            raise ValidationError(f'Город {new_name} уже в списке')
+
+        return new_name
--- StadyApp/weather/views.py
+++ StadyApp/weather/views.py
@@ -1,33 +1,51 @@
 from django.shortcuts import render
+from django.views import View
+
 from .models import City
 from pip._vendor import requests
 from .forms import CityForm
 
 
-def index(request):
-    appid = '5493ef258d707a2785561ade2a80bac3'
-    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=Metric&appid=' + appid
+class MainView(View):
 
-    if request.method == 'POST':
-        form = CityForm(request.POST)
-        form.save()
+    def post(self, request):
+        if request.POST.get('mybutton'):
+            City.objects.filter(name=request.POST.get('mybutton')).delete()
+        else:
+            form = CityForm(request.POST)
+            if form.is_valid():
+                form.save()
 
-    form = CityForm()
+        contex = self.api_speaker()
+        return render(request, 'index.html', contex)
 
-    cities = City.objects.all()
-    all_cities = []
-    for city in cities:
-        res = requests.get(url.format(city.name)).json()
+    def api_speaker(self):
+        appid = '5493ef258d707a2785561ade2a80bac3'
+        url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=Metric&appid=' + appid
 
-        city_info = {
-            'city': city.name,
-            'temp': res['main']['temp'],
-            'icon': res['weather'][0]['icon'],
+        form = CityForm()
+
+        cities = City.objects.all()
+        all_cities = []
+
+        for city in cities:
+            res = requests.get(url.format(city.name)).json()
+
+            city_info = {
+                'city': city.name,
+                'temp': res['main']['temp'],
+                'icon': res['weather'][0]['icon'],
+            }
+            all_cities.append(city_info)
+        contex = {
+            'all_info': all_cities,
+            'form': form,
         }
-        all_cities.append(city_info)
-    contex = {
-        'all_info': all_cities,
-        'form': form,
-    }
+        return  contex
 
-    return render(request, 'index.html', contex)
+    def get(self, request):
+        contex = self.api_speaker()
+
+        return render(request, 'index.html', contex)
+
+
